d304e6749c22fa6d0dab5291e2f0aeb9211ab69e
Óscar Sancho <oscar.sancho.gonzalez@gmail.com>
2023-11-30 00:49:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+
+const App = () => {
+  return (
+    <>
+      <h1>App</h1>
+      <Button variant='container'>Mi primer botón</Button>
+    </>
+  )
+}
+
+export default App
